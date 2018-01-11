d3ac666fe2c1100fba5bc309233839dd1fa81470
Dylan Tientcheu <dylanmambou17@gmail.com>
2018-01-11 13:24:17 +0100

    add sw

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", event => {
   fetchCuisines();
 
   // Add sw.js
-  // serviceWorkerRegister();
+  serviceWorkerRegister();
 });
 
 serviceWorkerRegister = () => {
