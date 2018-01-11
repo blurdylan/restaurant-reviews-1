e47fead45030ff321de57a859046c07a55ad76f4
Dylan Tientcheu <dylanmambou17@gmail.com>
2018-01-11 20:18:14 +0100

    fix the issues

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", event => {
   fetchCuisines();
 
   // Add sw.js
-  serviceWorkerRegister();
+  // ServiceWorkerRegister();
 });
 
 serviceWorkerRegister = () => {
@@ -165,7 +165,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = restaurant => {
   const li = document.createElement("li");
-  li.className = "resrev-half";
+  li.className = "resrev-quarter";
 
   const image = document.createElement("img");
   image.className = "restaurant-img";
